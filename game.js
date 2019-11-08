fab4c741a1d62818a7a7facbd57f1da57b9fbadd
Tim Dohmen <dohmentim@yahoo.com>
2019-11-08 13:48:46 -0700

    fighters

---
+++ game.js
@@ -0,0 +1,42 @@
+
+
+let enemies = {
+  D$: {
+    name: "D$",
+    health: 50,
+    moves: {
+      track: 0,
+      hunt: 5,
+      shank: 10
+    },
+    inventory: [],
+    timesHit: 0
+  },
+  Jake: {
+    name: "Jake",
+    health: 100,
+    moves: {
+      laughAtCode: 5,
+      ultimatePunch: 10,
+      whiteHatter: 15
+    },
+    inventory: [],
+    timesHit: 0
+  }
+}
+
+let player = {
+  name: "Mark",
+  health: 100,
+  moves: {
+    blameGame: 5,
+    breakCode: 10,
+    nat20: 20
+  },
+  invesntory: [],
+  timesHit: 0
+}
+
+let items = {
+
+}
